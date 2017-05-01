ae5ffec6b57c79f694c30aeb18db87631589948f
Patryk Skrzyniarz <patryk.skrzyniarz19@gmail.com>
2017-05-01 16:05:31 +0200

    Finished JS

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ var counterBtn = document.getElementsByClassName("btn-counter"),
     listItem = document.getElementsByTagName("li"),
     popularBtn = document.getElementById("popular");
 
-var refreshFilter = function(){
+var refreshFilter = function () {
+  "use strict";
   for (var i = 0; i < counterBtn.length; i++) {
     if (listItem[1].className === "active" && counterBtn[i].dataset.count === "0") {
       article[i].style.display = "none";
@@ -16,8 +17,22 @@ function favourite (e) {
   e.preventDefault();
   if (this.dataset.count >= 1){
     this.dataset.count--;
+    if (this.className === "btn btn-counter btn-active") {
+      this.className = "btn btn-counter";
+      this.childNodes[0].className = "fa fa-star fa-2x";
+    } else if (this.className === "img-note-btn btn-counter img-btn-active") {
+        this.className = "img-note-btn btn-counter";
+        this.childNodes[0].className = "fa fa-star fa-2x";
+    };
   } else {
     this.dataset.count++;
+    if (this.className === "btn btn-counter") {
+      this.className = "btn btn-counter btn-active";
+      this.childNodes[0].className = "fa fa-star fa-2x active-like";
+    } else if (this.className === "img-note-btn btn-counter") {
+        this.className = "img-note-btn btn-counter img-btn-active";
+        this.childNodes[0].className = "fa fa-star fa-2x active-like";
+    };
   };
   refreshFilter();
 };
@@ -25,7 +40,7 @@ function favourite (e) {
 function popularFilter (e) {
   e.preventDefault();
   listItem[0].className = "";
-  listItem[1].className = "active";
+  listItem[1].className = "active-link";
   for (var i = 0; i < article.length; i++) {
     for (var i = 0; i < counterBtn.length; i++) {
       if (counterBtn[i].dataset.count !== "0"){
@@ -40,7 +55,7 @@ function popularFilter (e) {
 function newestFilter (e) {
   e.preventDefault();
   listItem[1].className = "";
-  listItem[0].className = "active";
+  listItem[0].className = "active-link";
   for (var i = 0; i < article.length; i++) {
     article[i].style.display = "inline-block";
   };
