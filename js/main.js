1fdd9c78b7f6ceec71977154414e8a632112627b
Patryk Skrzyniarz <patryk.skrzyniarz19@gmail.com>
2017-05-01 16:40:32 +0200

    Finished JS

--- js/main.js
+++ js/main.js
@@ -7,15 +7,15 @@ var counterBtn = document.getElementsByClassName("btn-counter"),
 var refreshFilter = function () {
   "use strict";
   for (var i = 0; i < counterBtn.length; i++) {
-    if (listItem[1].className === "active" && counterBtn[i].dataset.count === "0") {
+    if (listItem[1].className === "active-link" && counterBtn[i].dataset.count === "0") {
       article[i].style.display = "none";
-    };
-  };
-};
+    }
+  }
+}
 
 function favourite (e) {
   e.preventDefault();
-  if (this.dataset.count >= 1){
+  if (this.dataset.count >= 1) {
     this.dataset.count--;
     if (this.className === "btn btn-counter btn-active") {
       this.className = "btn btn-counter";
@@ -23,7 +23,7 @@ function favourite (e) {
     } else if (this.className === "img-note-btn btn-counter img-btn-active") {
         this.className = "img-note-btn btn-counter";
         this.childNodes[0].className = "fa fa-star fa-2x";
-    };
+    }
   } else {
     this.dataset.count++;
     if (this.className === "btn btn-counter") {
@@ -32,10 +32,10 @@ function favourite (e) {
     } else if (this.className === "img-note-btn btn-counter") {
         this.className = "img-note-btn btn-counter img-btn-active";
         this.childNodes[0].className = "fa fa-star fa-2x active-like";
-    };
-  };
+    }
+  }
   refreshFilter();
-};
+}
 
 function popularFilter (e) {
   e.preventDefault();
@@ -43,14 +43,14 @@ function popularFilter (e) {
   listItem[1].className = "active-link";
   for (var i = 0; i < article.length; i++) {
     for (var i = 0; i < counterBtn.length; i++) {
-      if (counterBtn[i].dataset.count !== "0"){
+      if (counterBtn[i].dataset.count !== "0") {
         article[i].style.display = "inline-block";
       } else {
         article[i].style.display = "none";
-      };
-    };
-  };
-};
+      }
+    }
+  }
+}
 
 function newestFilter (e) {
   e.preventDefault();
@@ -58,13 +58,13 @@ function newestFilter (e) {
   listItem[0].className = "active-link";
   for (var i = 0; i < article.length; i++) {
     article[i].style.display = "inline-block";
-  };
-};
+  }
+}
 
 
 for (var i = 0; i < counterBtn.length; i++) {
-  counterBtn[i].addEventListener('click', favourite);
-};
+  counterBtn[i].addEventListener("click", favourite);
+}
 
-popularBtn.addEventListener('click', popularFilter);
-newestBtn.addEventListener('click', newestFilter);
+popularBtn.addEventListener("click", popularFilter);
+newestBtn.addEventListener("click", newestFilter);
